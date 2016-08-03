a3cfefdd65bb77b970a4546a848e19b7fe4754cc
cheungchan <1377699408@qq.com>
2016-08-03 19:38:03 +0800

    by CheungChan 分页查询

--- xuel-manager-web/src/main/java/com/xuel/manager/controller/UserController.java
+++ xuel-manager-web/src/main/java/com/xuel/manager/controller/UserController.java
@@ -16,46 +16,43 @@ import com.xuel.manager.service.DubboService;
 @RequestMapping("/user")
 @Controller
 public class UserController {
-	 @Autowired
-	 private DubboService  service;
-	 
-	 
-/**
-请求URL：/user/list
-请求参数：String page，Integer rows ，page和rows都是easyui分页控件提供的请求参数，其中page默认为1 ，rows是通过pagesize属性赋值的，也就是30
-请求返回值：json格式
-Easyui要求的json数据格式。{total:100,rows:[{},{}]}
-定义pojo来存储结果对象：DatagridResult对象，它包括Long total、List tows属性。
-业务逻辑：
-1、根据page和rows进行分页查询，需要使用pageHelper插件进行分页处理
-2、需要将查询的商品信息，封装到DatagridResult对象中，并且返回。
- * <p>Title: list</p>
- * <p>Description: </p>
- * @param page
- * @param rows
- * @return
- */	 
-	@RequestMapping("/list")
-	@ResponseBody
-	public DatagridResult list(@RequestParam(defaultValue="1")Integer page,Integer rows){
-		
-		return service.queryuserList(page, rows);
-	}
-	
+	@Autowired
+	private DubboService service;
+
+	/**
+	 * 请求URL：/user/list 请求参数：String page，Integer rows
+	 * ，page和rows都是easyui分页控件提供的请求参数，其中page默认为1 ，rows是通过pagesize属性赋值的，也就是30
+	 * 请求返回值：json格式 Easyui要求的json数据格式。{total:100,rows:[{},{}]}
+	 * 定义pojo来存储结果对象：DatagridResult对象，它包括Long total、List tows属性。 业务逻辑：
+	 * 1、根据page和rows进行分页查询，需要使用pageHelper插件进行分页处理
+	 * 2、需要将查询的商品信息，封装到DatagridResult对象中，并且返回。
+	 * 
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
 	@RequestMapping("/userlist")
-	public ModelAndView page(){
-   
-		List<User> userList = service.finduserList();
-		
-		//创建模型视图对象
-		
+	public ModelAndView velocityTest(
+			@RequestParam(defaultValue = "1") Integer page,
+			@RequestParam(defaultValue = "10") Integer rows) {
+		DatagridResult d = service.queryuserList(page, rows);
+		List<User> userList = d.getRows();
+		// 创建模型视图对象
+
 		ModelAndView modelAndView = new ModelAndView();
-		
-		//向模型和视图中添加数据用户返回给页面
-		modelAndView.addObject("userList", userList);
-		
-		modelAndView.setViewName("user/userlist");
-		
+		long total = d.getTotal() % rows == 0 ? d.getTotal() / rows : d
+				.getTotal() / rows + 1;
+		int prevPage = page == 1 ? 1 : page - 1;
+		int afterPage = page == (int) total ? (int) total : page + 1;
+		// 向模型和视图中添加数据用户返回给页面
+		modelAndView.addObject("userList", userList)//
+				.addObject("page", page)//
+				.addObject("prevPage", prevPage)//
+				.addObject("afterPage", afterPage)//
+				.addObject("total", total);//
+
+		modelAndView.setViewName("userlist");
+
 		return modelAndView;
 	}
 }
