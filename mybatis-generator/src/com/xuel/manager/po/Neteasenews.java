bc445c44884a706303dbeefa931f3ad943d6b1d2
cheungchan <1377699408@qq.com>
2016-08-04 10:31:10 +0800

    by CheungChan mybatis-generator

---
+++ mybatis-generator/src/com/xuel/manager/po/Neteasenews.java
@@ -0,0 +1,23 @@
+package com.xuel.manager.po;
+
+public class Neteasenews {
+    private String title;
+
+    private String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+}
